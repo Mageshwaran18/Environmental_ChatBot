e352aa09fd3ed118568bd035aa11028affc3baae
Mageshwaran <mageshwaranp18@gmail.com>
2024-03-25 19:28:29 +0530

    Name is being collected in the background !

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from fastapi.responses import JSONResponse
 import generic_helper
 
 # Carbon emission factors (in kgCO2 per km) for different transport modes
+inprogress_orders={}
+
 carbon_emission_factors = {
     "Car": 0.2,  # Example value, replace with actual data
     "Bus": 0.1,  # Example value, replace with actual data
@@ -24,28 +26,60 @@ async def handle_request(request: Request):
     intent_handler_dict = {
         'Calculate': calculate,
         'Car_or_Bike': calculate_for_car_or_bike,
+        'After_User_Name':add_name,
+        'Confirmation': check,
     }
 
-    return intent_handler_dict[intent](parameters)
+    session_id = generic_helper.extract_session_id(output_contexts[0]['name'])
+    return intent_handler_dict[intent](parameters,session_id)
 
-def calculate(parameters: dict):
+def check(parameters:dict , session_id: str):
+    if session_id not in inprogress_orders:
+         return JSONResponse(content={
+        "fulfillmentText": "I think you forgot to enter the Name. Please enter the name to proceed."
+    })
 
-    Distance = int(parameters["Distance"])
-    TransportMode = parameters["TransportMode"].capitalize()
-    carbon_emission = Distance * carbon_emission_factors.get(TransportMode, 0)
-    
+def calculate(parameters: dict,session_id: str):
+
+    print(session_id)
+    if session_id not in inprogress_orders:
+         return JSONResponse(content={
+        "fulfillmentText": "I think you forgot to enter the Name. Please enter the name to proceed."
+    })
+
+    else:
+        Distance = int(parameters["Distance"])
+        TransportMode = parameters["TransportMode"].capitalize()
+        carbon_emission = Distance * carbon_emission_factors.get(TransportMode, 0)
+        
     return JSONResponse(content={
         "fulfillmentText": f"Your carbon footprint for traveling {Distance} km by {TransportMode} is {carbon_emission:.2f} kgCO2."
     })
 
-def calculate_for_car_or_bike(parameters : dict):
+def calculate_for_car_or_bike(parameters : dict,session_id: str):
 
-    Distance = int(parameters["Distance"])
-    Car_Bike = parameters["Car_Bike"].capitalize()
-    fuel_efficiency = int(parameters["Efficiency"])
-    
+    print(session_id)
+    if session_id not in inprogress_orders:
+         return JSONResponse(content={
+        "fulfillmentText": "I think you forgot to enter the Name. Please enter the name to proceed."
+    })
+
+    else:
+        Distance = int(parameters["Distance"])
+        Car_Bike = parameters["Car_Bike"].capitalize()
+        fuel_efficiency = int(parameters["Efficiency"])
+        
     carbon_emission = Distance * (1 / fuel_efficiency)
     
     return JSONResponse(content={
         "fulfillmentText": f"Your carbon footprint for traveling {Distance} km by {Car_Bike} with an efficiency of {fuel_efficiency} km/l is {carbon_emission:.2f} kgCO2."
     })
+
+
+def add_name(parameters: dict,session_id: str):
+
+    name = parameters["Name"]['name']
+    print(session_id)
+    if session_id not in inprogress_orders:
+        inprogress_orders[session_id]=name
+    print(inprogress_orders)
